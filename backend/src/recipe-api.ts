f957d1377acfe4ce83546c92d912e05dd407400a
AmineBENCHOUCHE <amine.benchouche@gmail.com>
2023-11-16 11:48:21 +0100

    Add RecipeSummary on the backend

--- backend/src/recipe-api.ts
+++ backend/src/recipe-api.ts
@@ -28,3 +28,24 @@ export const searchRecipes = async (searchTerm: string, page: number) => {
     }
 }
 
+export const getRecipeSummary = async (recipeId: string) => {
+    //check if apikey is undefined because we cant send a query param undefined
+    if(!apiKey){
+        throw new Error('API KEY not found ')
+    }
+
+    const url = new URL(`https://api.spoonacular.com/recipes/${recipeId}/summary`)
+    const params = {
+        apiKey        
+    }
+    url.search = new URLSearchParams(params).toString()
+    
+    const response = await fetch(url)
+
+    if(!response.ok){
+        throw new Error(`HTTP error! Status: ${response.status}`)
+    }
+    const json = await response.json()
+
+    return json
+}
